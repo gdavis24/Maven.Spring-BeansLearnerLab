3b0f89cadf749f805dfe6f9ea1cb61072d939853
Greg Davis <jrobley3@hotmail.com>
2020-03-29 21:37:56 -0400

    issues with tests

--- src/test/java/com/example/demo/TestConfigurations/TestInstructorConfig.java
+++ src/test/java/com/example/demo/TestConfigurations/TestInstructorConfig.java
@@ -1,10 +1,34 @@
 package com.example.demo.TestConfigurations;
 
+import com.example.demo.models.Students;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestInstructorConfig {
+
+    @Autowired
+    @Qualifier("tcUsaInstructors")
+    Students currentStudents;
+
+    @Autowired
+    @Qualifier("tcUkInstructors")
+    Students previousStudents;
+
+    @Test
+    public void testCurrentStudents() {
+        Assert.assertTrue(currentStudents.findById(1L).getName().equals("Kris"));
+    }
+
+    @Test
+    public void testPreviousStudents() {
+        Assert.assertTrue(previousStudents.findById(1L).getName().equals("Jeff"));
+    }
+
 }
